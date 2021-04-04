f4134e41b16bc9d7fde5feba96c16ca4a6b6d16f
hisokaSV <kololi76@gmail.com>
2021-04-05 01:23:51 +0200

    initial commit

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -8,7 +8,7 @@ module.exports = class PauseCommand extends Command {
             aliases: ['c'],
             group: 'music',
             memberName: 'pause',
-            description: 'met la lecture en pause `-c` ou  `-pause`',
+            description: 'met la lecture en pause `-c` ou  `-pause`'
         });
     }
 
@@ -28,7 +28,7 @@ module.exports = class PauseCommand extends Command {
         }
 
         if (!message.client.voice.connections.first()) {
-            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `join` pour m'ajouter :x:")
+            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `join` pour m'ajouter :x:");
         }
           
         if(dispatcher){
