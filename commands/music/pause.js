3b5a4646fd6ab49f51a39c058cb9986317396f9d
hisokaSV <kololi76@gmail.com>
2021-04-05 06:20:03 +0200

    initial commit

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,3 +1,4 @@
+const { UserNotInVoiceChannel,BotNotInVoiceChannel } = require('../../strings.json');
 const { Command, CommandoMessage } = require("discord.js-commando");
 const { StreamDispatcher } = require('discord.js');
 
@@ -24,13 +25,13 @@ module.exports = class PauseCommand extends Command {
          const dispatcher = message.client.server.dispatcher;
 
         if (!message.member.voice.channel) {
-            return message.say(':x: Tu dois ètre dans un salon vocal pour utiliser cette commande :x:');
+            return message.say(UserNotInVoiceChannel);
         }
 
         if (!message.client.voice.connections.first()) {
-            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `join` pour m'ajouter :x:");
+            return message.say(BotNotInVoiceChannel);
         }
-          
+
         if(dispatcher){
            dispatcher.pause();
         }
